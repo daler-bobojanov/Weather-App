ef98c5ac8b892a8238878212cda7641fcf0d5ea7
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-04-12 19:31:30 -0400

    added className

--- weather-app/src/components/Titles.jsx
+++ weather-app/src/components/Titles.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Titles = () => (
     <div>
-        <h1>Five Day Weather Forecast</h1>
-        <p>Find out temperature condition and more...</p>
+        <h1 className="title-container__title">Five Day Weather Forecast</h1>
+        <p className="title-container__subtitle">Find out temperature condition and more...</p>
     </div>
 );
 
